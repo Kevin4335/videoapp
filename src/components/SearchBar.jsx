f34aac59268410dbe28c93a40e9b3570d7be76ff
Kevin4335 <kvchang@umich.edu>
2023-08-14 19:50:39 -0400

    todo:fix undefined search

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,10 +3,19 @@ import { useNavigate } from 'react-router-dom'
 import { Paper, IconButton } from '@mui/material'
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate=useNavigate();
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    if(searchTerm){
+      navigate(`/search/${searchTerm}`);
+      setSearchTerm('');
+    }
+  }
   return (
     <Paper
         component = "form"
-        onSubmit={()=>{}}
+        onSubmit={handleSubmit}
         sx={{
             borderRadius:20,
             border: '3px solid #2C1E74',
@@ -19,8 +28,8 @@ const SearchBar = () => {
         <input
             className='search-bar'
             placeholder='Search'
-            value=''
-            onChange={()=>{}}
+            value={searchTerm}
+            onChange={(e)=>setSearchTerm(e.target.value)}
         />
         <IconButton type='submit' sx={{p:'10px'}}> 
         <img src= {require("../utils/logos/magnifying-glass.png")} alt = "logo" height = {25} />
--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { Box,Stack, Typography } from '@mui/material';
+import {useParams} from 'react-router-dom';
+
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+import { Videos } from './';
 
 const SearchFeed = () => {
+
+  const[videos,setVideos] = useState([]);
+  const{searchTerm} = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
+      .then((data) => setVideos(data.items))
+    }, [searchTerm]);
+
   return (
-    <div>SearchFeed</div>
+    <Box p={2} sx= {{overflow: 'auto', height:'90vh',flex:2 }}>
+        <Typography variant = 'h4' fontWeight = 'bold' mb= '2' sx={{color: '#C23A73'}}>
+           Search results: <span style={{color: '#2C1E74'}}>
+            {searchTerm}</span> videos
+        </Typography>
+        <Videos videos={videos}/>
+      </Box>
   )
 }
 
